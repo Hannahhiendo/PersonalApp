0edc02b7fe9daf9fbe4230f958c56eb8a5ace297
Hien Do <hiendo@Hannahs-MacBook-Pro.local>
2020-09-27 14:49:27 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,124 @@
+const background = document.getElementById('Background')
+const thumbnail = document.getElementById('Thumbnail')
+const Song = document.getElementById('Song')
+
+const songartist = document.getElementById('song-artist')
+const songtitle = document.getElementById('song-title')
+
+const rangeslider = document.getElementById('range-slider')
+const playPause = document.getElementById('Play-Pause')
+
+
+const nextBtn = document.getElementById('Next')
+const previousBtn = document.getElementById('Previous')
+
+nextBtn.onclick = nextSong
+
+previousBtn.onclick = previousSong
+
+
+let artists = [
+    'Harry styles',
+    'Bts'
+]
+
+let titles = [
+    'Watermelon Sugar',
+    'Dynamite'
+]
+let songs = [
+    './assets/music/Watermelon Sugar - Harry Styles.mp3',
+    './assets/music/Dynamite - BTS.mp3'
+]
+let covers = [
+    './assets/cover/Fine Line album.jpg',
+    './assets/cover/dynamite.jpeg'
+]
+
+let playing = true
+
+function PlayPauseFunc() {
+    if(playing){
+        playPause.src = './assets/icon/pause.png'
+        Song.play()
+    }
+    else{
+        playPause.src = './assets/icon/play-button.png'
+        Song.pause()
+    }
+    playing = !playing
+}
+
+let songIndex = 0
+
+function nextSong(){
+
+    console.log('in next')
+
+    songIndex++;
+    if(songIndex >= songs.length){
+        songIndex = 0;
+
+    } 
+    Song.src = songs[songIndex]
+    Thumbnail.src = covers[songIndex]
+    Background.src = covers[songIndex]
+
+    document.getElementById('song-artist').innerHTML = artists[songIndex]
+    document.getElementById('song-title').innerHTML = titles[songIndex]
+
+
+    playing = true
+    PlayPauseFunc()
+    
+}
+
+function previousSong(){
+
+    console.log('in previous')
+    songIndex--;
+    if(songIndex <= 0){
+        songIndex = 0;
+    } 
+
+    Song.src = songs[songIndex]
+    Thumbnail.src = covers[songIndex]
+    Background.src = covers[songIndex]
+
+    document.getElementById('song-artist').innerHTML = artists[songIndex]
+    document.getElementById('song-title').innerHTML = titles[songIndex]
+
+
+    playing = true
+    PlayPauseFunc()
+}
+
+function formatTime(seconds) {
+    let minutes = Math.floor(seconds/60)
+    seconds = seconds % 60
+    if (seconds < 10){
+        seconds = `0${seconds}`
+    }
+    return `${minutes}:${seconds}`
+}
+
+rangeslider.onchange = () => {
+    Song.currentTime = rangeslider.value
+}
+
+function updateProgressValue() {
+    rangeslider.max = Song.duration
+    rangeslider.value = Song.currentTime
+
+    document.querySelector('.currenttime').innerHTML = formatTime(Math.floor(Song.currentTime))
+
+    if (document.querySelector('.durationtime').innerHTML == "NaN:NaN"){
+        document.querySelector('.durationtime').innerHTML = '0:00'
+    }
+    else {
+        document.querySelector('.durationtime').innerHTML = formatTime(Math.floor(Song.duration))
+
+    }
+}
+
+setInterval(updateProgressValue, 500);
